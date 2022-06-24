15646305d1d1d92619d788fd3f13106e7a95fc91
Honey Jayan <honeyjayan2000@gmail.com>
2022-06-24 10:45:01 -0400

    one last commit

--- server/routes/books.js
+++ server/routes/books.js
@@ -31,45 +31,97 @@ router.get('/', (req, res, next) => {
 
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+  res.render('books/details', {
+    title: 'Add book',
+    books: ''
+   });
 });
 
 // POST process the Book Details page and create a new Book - CREATE
 router.post('/add', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  let newBook = book({
+    "Title" : req.body.title,
+    "Description": req.body.description,
+    "Price": req.body.price,
+    "Author": req.body.author,
+    "Genre": req.body.genre
+});
+
+  book.create(newBook, (err, book)=>{
+  if(err)
+  {
+    console.log(err);
+    res.end(err);
+  }
+  else
+  {
+    res.redirect('/books');
+  }
+  });
 
 });
 
 // GET the Book Details page in order to edit an existing Book
 router.get('/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  let id = req.params.id;
+
+  book.findById(id, (err, bookToEdit) => {
+      if(err)
+      {
+          console.log(err);
+          res.end(err);
+      }
+      else
+      {
+          res.render('books/details', {title: 'Edit Contact', books : bookToEdit})
+      }
+  });
 });
 
 // POST - process the information passed from the details form and update the document
 router.post('/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  let id = req.params.id;
 
+  let updateBook = book({
+    "_id": id,
+    "Title" : req.body.title,
+    "Description": req.body.description,
+    "Price": req.body.price,
+    "Author": req.body.author,
+    "Genre": req.body.genre
+  });
+
+  book.updateOne({_id: id}, updateBook, (err) =>{
+    if(err)
+    {
+        console.log(err);
+        res.end(err);
+    }
+    else{
+        res.redirect('/books');
+    }
+  });
 });
 
 // GET - process the delete by user id
 router.get('/delete/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  let id = req.params.id;
+
+  book.remove({_id: id}, (err) => {
+      if(err)
+      {
+          console.log(err);
+          res.end(err);
+      }
+      else
+      {
+          res.redirect('/books');
+      }
+  });
 });
 
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -10,10 +10,9 @@ let router = express.Router();
 let mongoose = require('mongoose');
 const books = require('../models/books');
 
-// define the game model
+// define the model
 let book = require('../models/books');
 
-let bookController = require('../controller/book')
 
 /* GET home page. wildcard */
 router.get('/', (req, res, next) => {
@@ -22,25 +21,4 @@ router.get('/', (req, res, next) => {
     books: ''
    });
 });
-
-/* GET Route for displaying the Add page - CREATE Operation */
-router.get('/add', (req, res, next) => {
-  res.render('books/details', {
-    title: 'Add book',
-    books: ''
-   });
-});
-
-/* POST Route for processing the Add page - CREATE Operation */
-router.post('/add', bookController.processAddPage);
-
-/* GET Route for processing the Edit page - UPDATE Operation */
-router.get('/edit/:id', bookController.displayEditPage);
-
-/* POST Route for processing the edit page - UPDATE Operation */
-router.post('/edit/:id', bookController.processEditPage);
-
-/* Get Route for processing the Delete page - DELETE Operation */
-router.get('/delete/:id', bookController.performDelete);
-
 module.exports = router;
